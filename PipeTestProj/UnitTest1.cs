b42d2394e0f1e50b7f3c72853d271a922d641370
AntonVykhlo <antonvykhlo@gmail.com>
2020-08-09 17:21:16 +0300

    dummy test for pipe

---
+++ PipeTestProj/UnitTest1.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace PipeTestProj
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
